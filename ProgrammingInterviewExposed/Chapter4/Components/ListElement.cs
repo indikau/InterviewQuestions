52422c51ccaa4793e2b41e575715937115fa8d08
Indikau <indika@outlook.com>
2016-11-26 13:35:28 -0800

    Implement stack from Linked list

---
+++ ProgrammingInterviewExposed/Chapter4/Components/ListElement.cs
@@ -0,0 +1,17 @@
+﻿namespace ProgrammingInterviewExposed.Chapter4.Components
+{
+    public class ListElement<T>
+    {
+        public ListElement<T> Next { get; set; }
+
+        public T Value { get; set; }
+
+        public ListElement() { }
+
+        public ListElement(T value, ListElement<T> next)
+        {
+            Value = value;
+            Next = next;
+        }
+    }
+}
---
+++ ProgrammingInterviewExposed/Chapter4/Stack.cs
@@ -0,0 +1,33 @@
+﻿namespace ProgrammingInterviewExposed.Chapter4
+{
+    using System;
+    using Components;
+
+    public class Stack<T>
+    {
+        private ListElement<T> root;
+
+        public Stack(T value)
+        {
+            this.root = new ListElement<T>(value, null);
+        }
+
+        public void Push(ListElement<T> node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException("Given node is null, therefore we will not insert into the stack");
+            }
+
+            node.Next = root;
+            root = node;
+        }
+
+        public T Pop()
+        {
+            var top = root;
+            root = root.Next;
+            return top.Value;
+        }
+    }
+}
