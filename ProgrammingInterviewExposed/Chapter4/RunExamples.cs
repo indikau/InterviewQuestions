89350f542bf1dd8c808a0728fe0bd5aba059a81b
Indikau <indika@outlook.com>
2016-12-02 04:11:56 -0800

    add examples and comments.

--- ProgrammingInterviewExposed/Chapter4/RunExamples.cs
+++ ProgrammingInterviewExposed/Chapter4/RunExamples.cs
@@ -1,9 +1,42 @@
 ﻿namespace ProgrammingInterviewExposed.Chapter4
 {
+    using System;
+
     public class RunExamples
     {
+        private static Random rnd = new Random(2);
         public static void Main(string[] args)
         {
+            RunStackExamples();
+        }
+
+        private static void RunStackExamples()
+        {
+            // Create stack
+            Console.WriteLine("Create stack with value 10");
+            var stack = new Stack<int>(GetRandomIntValue());
+            // pop the stack
+            Console.WriteLine("\t<= Pop from the stack :{0}", stack.Pop());
+            try
+            {
+                Console.Write("\t<= Pop from the stack :");
+                Console.Write(stack.Pop());
+            }
+            catch (StackEmptyException e)
+            {
+                Console.WriteLine("\n\t\tStack is EMPTY: {0}",e.Message);
+            }
+            var value = GetRandomIntValue();
+            Console.WriteLine("\t=> Push to the stack :{0}", value);
+            stack.Push(value);
+            value = GetRandomIntValue();
+            Console.WriteLine("\t=> Push to the stack :{0}", value);
+            stack.Push(value);
+        }
+
+        private static int GetRandomIntValue()
+        {            
+            return rnd.Next(0, 100);
         }
     }
 }
--- ProgrammingInterviewExposed/Chapter4/Stack.cs
+++ ProgrammingInterviewExposed/Chapter4/Stack.cs
@@ -1,23 +1,38 @@
 ﻿namespace ProgrammingInterviewExposed.Chapter4
 {
-    using System;
     using Components;
-
+    /// <summary>
+    /// Implementation of the stack using linked list.
+    /// </summary>
+    /// <typeparam name="T">Type of the value</typeparam>
     public class Stack<T>
     {
         private ListElement<T> root;
 
+        /// <summary>
+        /// Initializing the class <see cref="Stack{T}"/>.
+        /// </summary>
+        /// <param name="value">Initial value.</param>
         public Stack(T value)
         {
             this.root = new ListElement<T>(value, null);
         }
 
+        /// <summary>
+        /// Push value to the stack
+        /// </summary>
+        /// <param name="value">Value to push to the stack</param>
         public void Push(T value)
         {
             var node = new ListElement<T>(value, root);
             root = node;
         }
 
+        /// <summary>
+        /// Pop the stack.
+        /// </summary>
+        /// <returns>Top value of the stack.</returns>
+        /// <exception cref="StackEmptyException">Throws when the stack is empty.</exception>
         public T Pop()
         {
             if (root == null)
