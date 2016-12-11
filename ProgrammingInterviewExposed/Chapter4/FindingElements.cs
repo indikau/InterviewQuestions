8754429204ff03a23fe87fe07b088638238ceb86
Indikau <indika@outlook.com>
2016-12-11 13:00:25 -0800

    Finding mth element.

---
+++ ProgrammingInterviewExposed/Chapter4/FindingElements.cs
@@ -0,0 +1,36 @@
+﻿namespace ProgrammingInterviewExposed.Chapter4
+{
+    using ProgrammingInterviewExposed.Chapter4.Components;
+
+    public class FindingElements<T>
+    {
+        public ListElement<T> FindMToLastElement(ListElement<T> head, int m)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            ListElement<T> curr, mBehind;
+            curr = head;
+            for (int i = 0; i < m; i++)
+            {
+                if (curr.Next != null)
+                {
+                    curr = curr.Next;
+                }else
+                {
+                    return null;
+                }
+            }
+
+            mBehind = curr;
+            while (curr.Next != null)
+            {
+                mBehind = mBehind.Next;
+                curr = curr.Next;
+            }
+            return mBehind;
+        }
+    }
+}
