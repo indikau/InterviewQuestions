cc63c29b467f646bc63564cb375f04b1992c89dd
Indikau <indika@outlook.com>
2016-12-11 14:20:50 -0800

    Flatten the list.

---
+++ ProgrammingInterviewExposed/Chapter4/Components/Node.cs
@@ -0,0 +1,22 @@
+﻿namespace ProgrammingInterviewExposed.Chapter4.Components
+{
+    public class Node<T>
+    {
+        public Node(T value, Node<T> next, Node<T> prev, Node<T> child)
+        {
+            Value = value;
+            Next = next;
+            Prev = prev;
+            Child = child;
+        }
+
+        public T Value { get; set; }
+
+        public Node<T> Next { get; set; }
+
+        public Node<T> Prev { get; set; }
+
+        public Node<T> Child { get; set; }
+
+    }
+}
---
+++ ProgrammingInterviewExposed/Chapter4/ListFlattening.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ProgrammingInterviewExposed.Chapter4.Components;
+
+namespace ProgrammingInterviewExposed.Chapter4
+{
+    public class ListFlattening<T>
+    {
+        public void FlattenList(Node<T> head,Node<T> tail)
+        {
+            Node<T> curr = head;
+            while (curr != null)
+            {
+                if (curr.Child != null)
+                {
+                    Append(curr.Child, tail);
+                }
+                curr = curr.Next;
+            }
+        }
+
+        private void Append(Node<T> child, Node<T> tail)
+        {
+            Node<T> curr;
+
+            tail.Next = child;
+            child.Prev = tail;
+
+            curr = child;
+
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+            }
+            tail = curr;
+        }
+    }
+}
