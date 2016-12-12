dec83367f6eafea3b678db1be703b301583d44fb
Indikau <indika@outlook.com>
2016-12-11 22:38:40 -0800

    Acyclic list.

---
+++ ProgrammingInterviewExposed/Chapter4/Acyclic.cs
@@ -0,0 +1,26 @@
+﻿using ProgrammingInterviewExposed.Chapter4.Components;
+
+namespace ProgrammingInterviewExposed.Chapter4
+{
+    public class Acyclic<T>
+    {
+        public bool IsAcyclic(ListElement<T> head)
+        {
+            ListElement<T> curr2 = head;
+            ListElement<T> curr = head;
+            if (curr.Next == null)
+                return true;
+
+            while (curr2.Next.Next != null)
+            {
+                curr2 = curr2.Next.Next;
+                curr = curr.Next;
+                if (curr == curr2)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ProgrammingInterviewExposed/Chapter4/ListUnflatten.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgrammingInterviewExposed.Chapter4
+{
+    using ProgrammingInterviewExposed.Chapter4.Components;
+    public class ListUnflatten<T>
+    {
+        public void UnFlattenList(Node<T> head, Node<T> tail)
+        {
+
+        }
+    }
+}
